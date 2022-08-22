67e47512a7a18fc074d218086575e1728b2db8e8
Cristiano Sales <cristianosalesneves@gmail.com>
2022-08-22 20:08:16 -0300

    04 - Crie uma rota para o endpoint /cars onde seja possível cadastrar um novo carro

--- src/app.ts
+++ src/app.ts
@@ -1,5 +1,11 @@
 import express from 'express';
+import router from './routes';
+import 'express-async-errors';
 
 const app = express();
+
+app.use(express.json());
+
+app.use(router);
 
 export default app;
