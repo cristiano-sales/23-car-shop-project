4af02c684765a4846a73d0023006084281cc8cfe
Cristiano Sales <cristianosalesneves@gmail.com>
2022-08-22 20:45:43 -0300

    mock cars

---
+++ src/tests/unit/mocks/cars.mock.ts
@@ -0,0 +1,20 @@
+export const mockValidCar = {
+	model: "modelo exemplo",
+	year: 1963,
+	color: "red",
+	buyValue: 3500,
+	seatsQty: 4,
+	doorsQty: 4,
+	status: true,
+}
+
+export const mockValidCarWithId = {
+	"_id": "82cf1fc6498565d94eba52cb",
+	"model": "modelo exemplo",
+	"year": 1963,
+	"color": "red",
+	"buyValue": 3500,
+	"doorsQty": 4,
+	"seatsQty": 4,
+	"status": true,
+}
