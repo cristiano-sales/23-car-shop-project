883ad00dc41283c2bcb8dc715b859b2ba076b79d
Cristiano Sales <cristianosalesneves@gmail.com>
2022-08-23 19:27:29 -0300

    adjust car controller

--- src/controllers/car.controller.ts
+++ src/controllers/car.controller.ts
@@ -1,15 +1,16 @@
-import { Request, Response } from 'express';
+import { Response } from 'express';
 import { ICar } from '../interfaces/ICar';
 import IService from '../interfaces/IService';
+import { IReqBody } from '../interfaces/IReqBody';
 
 class CarController {
   constructor(private _service: IService<ICar>) { }
 
   public create = async (
-    req: Request & { body: ICar },
+    req: IReqBody<ICar>,
     res: Response,
-  ) => {
-    const car = req.body;
+  ) : Promise<void> => {
+    const car: ICar = req.body;
 
     const carCreated: ICar = await this._service.create(car);
 
